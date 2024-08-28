198e7c171bb49f5d7c4fdf0a0d55ee0891ce6243
sekineseishu <mika242401@gmail.com>
2024-08-28 18:01:21 +0900

    test

--- Assets/キャラクター/Character.cs
+++ Assets/キャラクター/Character.cs
@@ -6,6 +6,7 @@ using UnityEngine.SceneManagement;
 public class Character : MonoBehaviour
 {
     public static Character Instance;
+    public MoveDirections moveDirection;
     public Transform targetPosition;
     public Transform defaultRotation;
     private float movespeed = 0.7f;
@@ -14,8 +15,8 @@ public class Character : MonoBehaviour
     public bool blueGoal;
     public bool red;
     public bool blue;
-    public int nowXpos;
-    public int nowZpos;
+    public float nowXpos;
+    public float nowZpos;
     public bool OnWorp;
 
     public void Awake()
@@ -28,104 +29,8 @@ public class Character : MonoBehaviour
         redGoal = false;
         blueGoal = false;
         animator = GetComponent<Animator>();
-        nowXpos = (int)transform.position.x;
-        nowZpos = (int)transform.position.z;
-    }
-
-    //それぞれの移動可能方向一覧
-    public List<Vector3> GetValidMove()
-    {
-        List<Vector3> validMoves = new List<Vector3>();
-        if (blue)
-        {
-            Debug.Log("x:" + nowXpos + "z:" + nowZpos);
-            if (SceneManager.GetActiveScene().name == "stage")
-            {
-                //上
-                validMoves.Add(new Vector3(nowXpos, 0.5f, nowZpos + 1));
-                //下
-                validMoves.Add(new Vector3(nowXpos, 0.5f, nowZpos - 1));
-                //右
-                validMoves.Add(new Vector3(nowXpos + 1, 0.5f, nowZpos));
-                //左
-                validMoves.Add(new Vector3(nowXpos - 1, 0.5f, nowZpos));
-            }
-            else if (SceneManager.GetActiveScene().name == "Stage2")
-            {
-                //左上
-                validMoves.Add(new Vector3(nowXpos - 1, 0.5f, nowZpos + 1));
-                //左下
-                validMoves.Add(new Vector3(nowXpos - 1, 0.5f, nowZpos - 1));
-                //右
-                validMoves.Add(new Vector3(nowXpos + 1, 0.5f, nowZpos));
-            }
-            else if (SceneManager.GetActiveScene().name == "Stage3")
-            {
-                //上
-                validMoves.Add(new Vector3(nowXpos, 0.5f, nowZpos + 1));
-                //右
-                validMoves.Add(new Vector3(nowXpos + 1, 0.5f, nowZpos));
-                //左
-                validMoves.Add(new Vector3(nowXpos - 1, 0.5f, nowZpos));
-                //左下
-                validMoves.Add(new Vector3(nowXpos - 1, 0.5f, nowZpos - 1));
-                //右下
-                validMoves.Add(new Vector3(nowXpos + 1, 0.5f, nowZpos - 1));
-            }
-        }
-        if (red)
-        {
-            if (SceneManager.GetActiveScene().name == "stage")
-            {
-                //右上
-                validMoves.Add(new Vector3(nowXpos + 1, 0.5f, nowZpos + 1));
-                //左上
-                validMoves.Add(new Vector3(nowXpos - 1, 0.5f, nowZpos + 1));
-                //左下
-                validMoves.Add(new Vector3(nowXpos - 1, 0.5f, nowZpos - 1));
-                //右下
-                validMoves.Add(new Vector3(nowXpos + 1, 0.5f, nowZpos - 1));
-            }
-            else if (SceneManager.GetActiveScene().name == "Stage2")
-            {
-                //右上
-                validMoves.Add(new Vector3(nowXpos + 1, 0.5f, nowZpos + 1));
-                //右下
-                validMoves.Add(new Vector3(nowXpos + 1, 0.5f, nowZpos - 1));
-                //左
-                validMoves.Add(new Vector3(nowXpos - 1, 0.5f, nowZpos));
-            }
-            else if (SceneManager.GetActiveScene().name == "Stage3")
-            {
-                //右上
-                validMoves.Add(new Vector3(nowXpos + 1, 0.5f, nowZpos + 1));
-                //左上
-                validMoves.Add(new Vector3(nowXpos - 1, 0.5f, nowZpos + 1));
-                //下
-                validMoves.Add(new Vector3(nowXpos, 0.5f, nowZpos - 1));
-                //右
-                validMoves.Add(new Vector3(nowXpos + 1, 0.5f, nowZpos));
-                //左
-                validMoves.Add(new Vector3(nowXpos - 1, 0.5f, nowZpos));
-            }
-        }
-        return validMoves;
-    }
-
-    //移動可能方向
-    public void AddMove(int x,int z,List<Vector3Int>vailMoves)
-    {
-        vailMoves.Add(new Vector3Int(x, z));
-    }
-
-    //移動位置の取得
-    public void MoveCharacter(Transform targetpos)
-    {
-        targetPosition.position = targetpos.position;
-        transform.position = targetPosition.position;
-        nowXpos = (int)transform.position.x;
-        nowZpos = (int)transform.position.z;
-        Debug.Log("x:" + nowXpos + "z:" + nowZpos);
+        nowXpos = transform.position.x;
+        nowZpos = transform.position.z;
     }
 
     // Update is called once per frame
@@ -134,7 +39,7 @@ public class Character : MonoBehaviour
         //キャラクターの移動
         if (targetPosition != null)
         {
-            Debug.Log("aiueo");
+            Debug.Log("移動中！");
             transform.LookAt(targetPosition);
             animator.SetBool("run", true);
             transform.position = Vector3.MoveTowards(transform.position, targetPosition.position, movespeed * Time.deltaTime);
