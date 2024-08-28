b9a4e462490f038d5b8bc4700e1023210e7527a8
sekineseishu <mika242401@gmail.com>
2024-08-28 23:03:34 +0900

    隠しブロックやワープといったギミック

--- Assets/Script/Gimic/Gimic.cs
+++ Assets/Script/Gimic/Gimic.cs
@@ -5,74 +5,72 @@ using UnityEngine.SceneManagement;
 
 public class Gimic : MonoBehaviour
 {
-    public static Gimic Instance; 
-    public GameObject activeGimic;
-    public GameObject worp;
-    private GameObject chara;
-    private AudioSource audio;
-    public bool OnWorp;
-    private Character character;
+    public static Gimic Instance;
+    public GameObject activeGimic;//ブロック等を出現させるギミック
+    public GameObject worp;//ワープギミック
+    private AudioSource audio;//SEを流す先
+    public bool onWorp;//ワープフラグ
+    private Character character;//キャラクター
 
     public void Awake()
     {
         Instance = this;
     }
 
-    private void OnTriggerEnter(Collider other)
-    {
-        Character c = other.gameObject.GetComponent<Character>();
-        if (c.red && gameObject.tag == "RedGoal")
-        {
-                audio.Play();
-                activeGimic.SetActive(true);
-            if (SceneManager.GetActiveScene().name == "stage")
-            {
-                BloomManager.Instance.tutorialText4.SetActive(false);
-            }
-        }
-        if (c.blue && gameObject.tag == "BlueGoal")
-        {
-            audio.Play();
-            activeGimic.SetActive(true);
-        }
-        if (gameObject.tag == "worp")
-        {
-            if (other.gameObject.tag == "Character")
-            {
-                if (SceneManager.GetActiveScene().name == "Stage2" || SceneManager.GetActiveScene().name == "Stage3")
-                {
-                    if (c.OnWorp && GetWorp(worp.transform))
-                    {
-                        chara = other.gameObject;
-                        character = other.GetComponent<Character>();
-                        Invoke("Worp", 1);
-                    }
-                }
-            }
-        }
-    }
-
-    public void Worp()
-    {
-        if (worp.activeInHierarchy)
-        {
-            audio.Play();
-            chara.transform.position = worp.transform.position;
-            character.nowXpos = (int)chara.gameObject.transform.position.x;
-            character.nowZpos = (int)chara.gameObject.transform.position.z;
-            character.OnWorp = false;
-        }
-    }
-    // Start is called before the first frame update
     void Start()
     {
         audio = GetComponent<AudioSource>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter(Collider other)
     {
-        
+        Character character = other.gameObject.GetComponent<Character>();
+        //赤キャラクターが赤ギミックを踏んだら実行
+        if (character.red && gameObject.tag == "redGimic")
+        {
+            //隠していたオブジェクトを表示する
+            audio.Play();
+            activeGimic.SetActive(true);
+            Debug.Log("赤ギミック起動");
+            //チュートリアルステージだったら説明を消す
+            if (SceneManager.GetActiveScene().name == "stage")
+            {
+                tutorialManager.instance.nextTutorial();
+            }
+        }
+        //青キャラクターが青ギミックを踏んだら実行
+        if (character.blue && gameObject.tag == "blueGimic")
+        {
+            audio.Play();
+            activeGimic.SetActive(true);
+        }
+        //ワープギミック
+        if (gameObject.tag == "worp")
+        {
+            if (other.gameObject.tag == "Character")
+            {
+                //ワープ可能＆ワープ先にキャラクターがいなかったら実行
+                if (character.OnWorp && GetWorp(worp.transform))
+                {
+                    this.character = other.GetComponent<Character>();
+                    Invoke("Worp", 1);
+                }
+            }
+        }
+    }
+
+    //プレイヤーをワープする
+    public void Worp()
+    {
+        if (worp.activeInHierarchy)
+        {
+            //キャラクターの現在位置をワープ先に更新する
+            audio.Play();
+            character.transform.position = worp.transform.position;
+            character.nowXpos = (int)character.gameObject.transform.position.x;
+            character.nowZpos = (int)character.gameObject.transform.position.z;
+            character.OnWorp = false;
+        }
     }
 
     private bool GetWorp(Transform worpPosition)
@@ -88,5 +86,11 @@ public class Gimic : MonoBehaviour
             }
         }
         return true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
     }
 }
