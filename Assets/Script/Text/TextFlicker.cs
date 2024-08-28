d78e5af39f5a97046fd5dc90cb5e3e56f03e96ac
sekineseishu <mika242401@gmail.com>
2024-08-28 23:05:04 +0900

    テキストの点滅

--- Assets/Script/Text/TextFlicker.cs
+++ Assets/Script/Text/TextFlicker.cs
@@ -6,23 +6,23 @@ using TMPro;
 
 public class TextFlicker : MonoBehaviour
 {
-    TextMeshProUGUI tmp;
+    TextMeshProUGUI text;//点滅させるテキスト
     public float a;
     public float b = 1.0f;
     [SerializeField]
-    Color32 startColor = new Color32(255, 50, 68,0);
+    Color32 startColor = new Color32(255, 50, 68,0);//最初の色
     [SerializeField]
-    Color32 endColor = new Color32(151,0,18,0);
+    Color32 endColor = new Color32(151,0,18,0);//最後の色
     // Start is called before the first frame update
     void Start()
     {
-        tmp = GetComponent<TextMeshProUGUI>();
+        text = GetComponent<TextMeshProUGUI>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        tmp.alpha = a;//変数aのalpha値を変更
+        text.alpha = a;//変数aのalpha値を変更
         a += b * Time.deltaTime;
         if (a >= 1f)
         {
